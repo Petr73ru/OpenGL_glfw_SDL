20344aa7251ff95aed0174ddd483f3a9a69b951b
Petr73ru <tank73ru@gmail.com>
2021-07-01 23:15:22 +0400

    Start of app

--- OpenGL_SDL/main.cpp
+++ OpenGL_SDL/main.cpp
@@ -4,18 +4,11 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
-#include <cmath>
-
 #include "shader.hpp"
 
 SDL_Window* initWindow();
 void initVBO();
-bool events(SDL_Event e, bool &run, glm::vec3 &cameraPos, glm::vec3 &cameraFront, glm::vec3 &cameraUp);
-int genTexture(const char* name);
+bool events(SDL_Event e, bool &run);
 
 int main(int argc, char **argv){
   SDL_Window* window = initWindow();
@@ -23,248 +16,83 @@ int main(int argc, char **argv){
 
   Shader ourShader("vertex.glsl", "fragment.glsl");
 
-  glm::mat4 model = glm::mat4(1.0f);
-  glm::mat4 view = glm::mat4(1.0f);
-  view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-  glm::mat4 projection;
-  projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
-
-  int modelLoc = glGetUniformLocation(ourShader.ID, "model");
-  int viewLoc = glGetUniformLocation(ourShader.ID, "view");
-  int projectionLoc = glGetUniformLocation(ourShader.ID, "projection");
-
-  unsigned int texture1 = genTexture("container.jpg");
-  unsigned int texture2 = genTexture("awesomeface.png");
-  unsigned int texture3 = genTexture("video/1.jpg");
-  unsigned int texture4 = genTexture("video/2.jpg");
-
-  glEnable(GL_DEPTH_TEST);
-
-  glm::vec3 cubePositions[] = {
-    glm::vec3( 0.0f,  0.0f,  0.0f), 
-    glm::vec3( 2.0f,  5.0f, -15.0f), 
-    glm::vec3(-1.5f, -2.2f, -2.5f),  
-    glm::vec3(-3.8f, -2.0f, -12.3f),  
-    glm::vec3( 2.4f, -0.4f, -3.5f),  
-    glm::vec3(-1.7f,  3.0f, -7.5f),  
-    glm::vec3( 1.3f, -2.0f, -2.5f),  
-    glm::vec3( 1.5f,  2.0f, -2.5f), 
-    glm::vec3( 1.5f,  0.2f, -1.5f), 
-    glm::vec3(-1.3f,  1.0f, -1.5f)  
-  };
-
   SDL_Event e;
   bool run = true;
 
-  glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
-  glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-  glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
-
-  int frame = 0;
-  
-  while(run) {
-    frame++;
-    if(frame == 201) frame = 1;
-    events(e, run, cameraPos, cameraFront, cameraUp);
+  while(run){
+    events(e, run);
 
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    //    glBindTexture(GL_TEXTURE_2D, texture);
     ourShader.use();
 
-    const float radius = 10.0f;
-    float camX = sin(SDL_GetTicks() / 100) * radius;
-    float camZ = cos(SDL_GetTicks() / 100) * radius;
-    view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
-    
-    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-    glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
-    
-    // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
-    // draw cubes
-    for(unsigned int i = 0; i < 10; i++) {
-      if(i % 2 == 0) {
-	if(frame < 100) glBindTexture(GL_TEXTURE_2D, texture3); //glBindTexture(GL_TEXTURE_2D, texture1);
-	else glBindTexture(GL_TEXTURE_2D, texture4);
-      }
-      if(i % 2 != 0) glBindTexture(GL_TEXTURE_2D, texture2);
-      model = glm::mat4(1.0f);
-      model = glm::translate(model, cubePositions[i]);
-      float angle = 20.0f * i;
-      model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-
-      glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-
-      glDrawArrays(GL_TRIANGLES, 0, 36);
-    }
-    
     SDL_GL_SwapWindow(window);
   }
-
-// std::cout << glGetString(GL_VERSION) << std::endl;
 
   SDL_DestroyWindow(window);
 
   SDL_Quit();
-  
+
   return 0;
 }
 
-bool events(SDL_Event e, bool &run, glm::vec3 &cameraPos, glm::vec3 &cameraFront, glm::vec3 &cameraUp) {
-
-  const float cameraSpeed = 0.55f;
-  
-  while(SDL_PollEvent(&e)) {
-    if(e.type == SDL_QUIT) {
-      run = false;
-    } else if(e.type == SDL_KEYDOWN)
-    {
-      switch(e.key.keysym.sym)
-      {
-        case SDLK_ESCAPE: run = false; break;
-
-      case SDLK_w: cameraPos += cameraSpeed * cameraFront; break;
-      case SDLK_s: cameraPos -= cameraSpeed * cameraFront; break;
-      case SDLK_a: cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed; break;
-      case SDLK_d: cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed; break;
-      }
-    }
-  }
-  return run;
-}
-
-int genTexture(const char* name){
-  unsigned int texture;
-  glGenTextures(1, &texture);
-  glBindTexture(GL_TEXTURE_2D, texture);
-  // set the texture wrapping/filtering options (on the currently bound object)
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  // load and generate the texture
-  SDL_Surface* image = IMG_Load(name);
-
-  if(image) {
-    int mode = GL_RGB;
-
-    if(image->format->BytesPerPixel == 4){
-      mode = GL_RGBA;
-    }
-
-    glTexImage2D(GL_TEXTURE_2D, 0, mode, image->w, image->h, 0, mode, GL_UNSIGNED_BYTE, image->pixels);
-    glGenerateMipmap(GL_TEXTURE_2D);
-  } else {
-    std::cout << "Failed to load texture" << std::endl;
-  }
-
-  SDL_FreeSurface(image);
-  return texture;
-}
-
 void initVBO(){
-  /*float vertices[] = {
-    0.5f, 0.5f, 0.0f,     1.0f, 1.0f,
-    0.5f, -0.5f, 0.0f,    1.0f, 0.0f,
-    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f,
-    -0.5f, 0.5f, 0.0f,    0.0f, 1.0f
-  };*/
-  
   float vertices[] = {
-    -0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-     0.5f,  0.5f, -0.5f,  0.0f, 0.0f,
-     0.5f,  0.5f, -0.5f,  0.0f, 0.0f,
-    -0.5f,  0.5f, -0.5f,  1.0f, 0.0f,
-    -0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-
-    -0.5f, -0.5f,  0.5f,  1.0f, 1.0f,
-     0.5f, -0.5f,  0.5f,  0.0f, 1.0f,
-     0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-     0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-    -0.5f, -0.5f,  0.5f,  1.0f, 1.0f,
-
-    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-
-    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+     0.5f,  0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f,
+    -0.5f,  0.5f, 0.0f
   };
 
   unsigned int indices[] = {
-    0, 1, 3, 
+    0, 1, 3,
     1, 2, 3
   };
-  
-  unsigned int EBO;
+
+  unsigned int VBO, VAO, EBO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
   glGenBuffers(1, &EBO);
+
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-  
-  unsigned int VBO;
-  glGenBuffers(1, &VBO);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-  
-  unsigned int VAO;
-  glGenVertexArrays(1, &VAO);
 
-  glBindVertexArray(VAO);
-  
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-  // position attribute
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
-  // color attribute
-  //glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-  //glEnableVertexAttribArray(1);
-  // texture coord attribute
-  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-  glEnableVertexAttribArray(1);
+
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0);
 
   glBindVertexArray(VAO);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+}
 
-  // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+bool events(SDL_Event e, bool &run){
+  while(SDL_PollEvent(&e)){
+    if(e.type == SDL_QUIT) run = false;
+    else if(e.type == SDL_KEYDOWN){
+      switch(e.key.keysym.sym){
+      case SDLK_ESCAPE: run = false; break;
+      }
+    }
+  }
 
-  /* int nrAttributes;
-  glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
-  std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;*/
+  return run;
 }
 
 SDL_Window* initWindow() {
   SDL_Init(SDL_INIT_VIDEO);
 
-  SDL_Window *window = SDL_CreateWindow("OpenGL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_OPENGL /*| SDL_WINDOW_FULLSCREEN_DESKTOP*/);
+  SDL_Window *window = SDL_CreateWindow("OpenGL UI", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_OPENGL /* | SDL_WINDOW_FULLSCREEN_DESKTOP*/);
 
-  SDL_Surface* icon = IMG_Load("awesomeface.png");
+  SDL_Surface* icon = IMG_Load("img/2.jpg");
 
   SDL_SetWindowIcon(window, icon);
 
